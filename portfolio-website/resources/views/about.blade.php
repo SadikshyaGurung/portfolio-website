ea6ecd203b7146b2bdd549348a3f93c827e49839
gdnepal <gdnepal6@gmail.com>
2025-06-16 07:38:08 +0545

    Prepare to switch from akschit to about branch

---
+++ portfolio-website/resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+     <head>
+     <meta charset="UTF-8">
+        <title>About Us</title>
+        <link rel="stylesheet" href="{{asset(path: 'css/about.css')}}">
+     </head>
+    <body>
+      <h1>About Us</h1>
+
+        <div class="container">
+          <p>Welcome to our website! We are passionate about [your topic or business].Founded in [year], we have grown into a team of dedicated individuals who strive to make a difference.<br> Thank you for being part of our journey! Contact us: <a href="mailto:info@example.com">info@example.com</a></p>
+         </div>
+     </body>
+</html>
